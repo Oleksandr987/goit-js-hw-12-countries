6e47664ae2a57c507d3c58898f6cbb463a9e8abe
Oleksandr Kryvenets <kralvi987@gmail.com>
2019-12-16 23:33:18 +0200

    added 12

---
+++ src/js/app.js
@@ -0,0 +1,53 @@
+import { getCountries } from './services/api';
+import searchResultsListItemsTemplate from './templates/searchresults-list-items.hbs';
+import countryDescriptionTemplate from './templates/country-description.hbs';
+import { pWarning, pNotice } from './utils/pnotify';
+import { messages } from './utils/messages';
+import { refs } from './utils/refs';
+
+const debounce = require('lodash.debounce');
+
+function insertListItems(items) {
+  refs.alertList.insertAdjacentHTML('beforeend', items);
+}
+
+function insertCountryDescription(item) {
+  refs.foundContry.insertAdjacentHTML('afterbegin', item);
+}
+
+function removeListItems() {
+  refs.alertList.innerHTML = '';
+  refs.foundContry.innerHTML = '';
+}
+
+function searchFormSubmitHandler(event) {
+  event.preventDefault();
+  const form = event.target;
+  const inputValue = form.value.toLowerCase();
+  getCountries(inputValue)
+    .then(countries =>
+      countries.filter(country =>
+        country.name.toLowerCase().includes(inputValue),
+      ),
+    )
+    .then(result => {
+      const resultArr = Array.from(result);
+      if (resultArr.length === 0) {
+        pWarning(messages.warningMissingMatches);
+      } else if (inputValue.length === 0) {
+        removeListItems();
+      } else if (resultArr.length === 1) {
+        removeListItems();
+        insertCountryDescription(countryDescriptionTemplate(result[0]));
+      } else if (resultArr.length > 1 && resultArr.length <= 10) {
+        removeListItems();
+        insertListItems(searchResultsListItemsTemplate(result));
+      } else {
+        removeListItems();
+        pNotice(messages.warningTooManyMatches);
+      }
+    })
+    .catch(err => console.warn(err));
+}
+
+refs.input.addEventListener('input', debounce(searchFormSubmitHandler, 500));
