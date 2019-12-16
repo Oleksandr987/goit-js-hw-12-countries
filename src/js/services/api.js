725d97c078939dc730aa2b43a8b972f33942a813
Oleksandr Kryvenets <kralvi987@gmail.com>
2019-12-17 00:26:57 +0200

    added 5

--- src/js/services/api.js
+++ src/js/services/api.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable import/prefer-default-export */
-const URL = 'https://restcountries.eu/rest/v2/all';
+const URL = 'https://restcountries.eu/rest/v2/';
 
 const getCountries = () =>
   fetch(URL).then(response => response.json());
